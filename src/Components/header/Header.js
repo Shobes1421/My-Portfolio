6dc1266ff923392d543dd01887a5ae7b6f8e7bc1
David Shober <shobes1421@hotmail.com>
2020-08-10 23:15:26 -0600

    pushing up app

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -7,10 +7,10 @@ function Header() {
         <div className='container-fluid'>
             <div className='row'>
                 <div className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 myName'>
-                    <h1>David Shober</h1>
                 </div>
 
                 <div className='"col-xs-12 col-sm-12 col-md-6 col-lg-8 col-xl-8 navHeader'>
+                    <h1>David Shober</h1>
                     <span className="navbar-brand mr-2 h1 text-dark navlink"><Link to="/about">About</Link></span>
                     <span className="navbar-brand mr-2 h1 text-dark navlink"><Link to="/portfolio">Portfolio</Link></span>
                     <span className="navbar-brand ml h1 text-dark navlink"><Link to="/contact">Contact</Link></span>
